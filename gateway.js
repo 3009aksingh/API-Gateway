c61d24d83968ceb657653b6bc5ad88bd7f781c97
Ankitkumar2 Singh <Ankitkumar2.Singh@ril.com>
2023-05-17 17:38:16 +0530

    Till part-4 completed

--- gateway.js
+++ gateway.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
 const app = express();
 const PORT = 3000;
 const routes = require('./routes');
-// const app = express();
+
 app.use('/', routes);
+
 app.listen(PORT, () => {
   console.log('listening on port ' + PORT);
 });
